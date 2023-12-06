da16eb4baae688275c4fefad167af34ae1b24849
Shashank Naik <shashanknaik808@gmail.com>
2023-12-06 17:09:16 +0530

    Defined columns for the data grid

--- mui-tut/src/Test.jsx
+++ mui-tut/src/Test.jsx
@@ -1,8 +1,33 @@
 import React from 'react';
+import { DataGrid } from '@mui/x-data-grid';
+import { Box } from '@mui/material';
+
+const columns = [
+    { field: 'id', headerName: 'ID', width: 70 },
+    { field: 'firstName', headerName: 'First name', width: 130 },
+    { field: 'lastName', headerName: 'Last name', width: 130 },
+    {
+        field: 'age',
+        headerName: 'Age',
+        type: 'number',
+        width: 90,
+    },
+    {
+        field: 'fullName',
+        headerName: 'Full name',
+        description: 'This column has a value getter and is not sortable.',
+        sortable: false,
+        width: 160,
+        valueGetter: (params) =>
+            `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+    },
+];
 
 function Test() {
     return (
-        <div>Test</div>
+        <Box>
+            <DataGrid columns={ } />
+        </Box>
     )
 }
 
